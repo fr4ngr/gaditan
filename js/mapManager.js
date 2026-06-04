6fe5d36ec635b222c2971d3c2197696ecb56ae59
Fran CadizTaxi <fr4n.cadizpass@gmail.com>
2026-06-04 20:49:43 +0200

    UI: Renombrado opción del selector a Selecciona una parada

--- js/mapManager.js
+++ js/mapManager.js
@@ -208,7 +208,7 @@ const mapManager = (() => {
 
         const defaultOption = document.createElement('option');
         defaultOption.value = "";
-        defaultOption.innerText = "Elige tu parada";
+        defaultOption.innerText = "Selecciona una parada";
         defaultOption.disabled = true;
         defaultOption.selected = true;
         select.appendChild(defaultOption);
