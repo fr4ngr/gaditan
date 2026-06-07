e062c9df217a318155a14ac8a25cb05c7338a815
Fran CadizTaxi <fr4n.cadizpass@gmail.com>
2026-06-07 08:36:02 +0200

    chore: Migrate to Astro structure

--- extract.py
+++
@@ -1,9 +0,0 @@
-﻿import re
-
-with open("index.html", "r", encoding="utf-8") as f:
-    html = f.read()
-
-cards = re.findall(r'<div class="mini-dest-card".*?<div class="mini-dest-name">.*?</i>\s*(.*?)\s*</div>.*?<div class="mini-dest-price">(.*?)</div>', html, re.DOTALL)
-
-for c in cards:
-    print(f"{c[0]} | {c[1]}")
