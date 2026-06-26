5e8c7e9495a343c67ac9b16d4cb68d4274e79a70
Fran CadizTaxi <fr4n.cadizpass@gmail.com>
2026-06-26 13:22:00 +0200

    Pivot to cadiz.chat conversational UI

--- read_xlsx.cjs
+++
@@ -1,6 +0,0 @@
-const xlsx = require('xlsx');
-const workbook = xlsx.readFile('C:/Users/frn/Downloads/LISTADO PARADAS DE TAXI CADIZ.xlsx');
-const sheetName = workbook.SheetNames[0];
-const sheet = workbook.Sheets[sheetName];
-const data = xlsx.utils.sheet_to_json(sheet);
-console.log(JSON.stringify(data, null, 2));
